9364b4fe6f179c9b204918cf50ebbd2a25985478
твое имя < >
2024-01-24 19:40:14 +0400

    создала компонент PopExit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import RenderPopUserSet from './components/PopUserSet/PopUserSet'
+import RenderPopExit from './components/PopExit/PopExit'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,21 +12,7 @@ function App() {
 		 {
      //<!-- pop-up start-->
       }
-			<div className="pop-exit" id="popExit">
-				<div className="pop-exit__container">
-					<div className="pop-exit__block">
-						<div className="pop-exit__ttl">
-							<h2>Выйти из аккаунта?</h2>
-						</div>
-						<form className="pop-exit__form" id="formExit" action="#">
-							<div className="pop-exit__form-group">
-								<button className="pop-exit__exit-yes _hover01" id="exitYes"><a href="modal/signin.html">Да, выйти</a> </button>
-								<button className="pop-exit__exit-no _hover03" id="exitNo"><a href="main.html">Нет, остаться</a> </button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
+			<RenderPopExit />
 
 			<div className="pop-new-card" id="popNewCard">
 				<div className="pop-new-card__container">
